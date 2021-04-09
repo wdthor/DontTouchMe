82870d277c5cb4a4cf8faefc9c9aeccbf2150736
wdthor <wdthor@protonmail.com>
2021-04-09 02:28:59 +0200

    Add camera following an object using cinemachine Add collision physics

---
+++ Assets/ObjectHit.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHit : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log("Bumped into a wall");
+    }
+}
