79bfc112904795472b360658cfcdf7f2bd712fb6
wdthor <wdthor@protonmail.com>
2021-04-09 02:27:12 +0200

    Move instructions into methods Add movement behavior using the keyboard

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-
-    [SerializeField] float xValue = 0;
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zValue = 0;
+    [SerializeField] float moveSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(xValue, yValue, zValue);
+        MovePlayer();
+    }
+
+    void PrintInstructions() {
+        Debug.Log("Welcome to the game");
+    }
+    
+    void MovePlayer() {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, 0, zValue);
     }
 }
